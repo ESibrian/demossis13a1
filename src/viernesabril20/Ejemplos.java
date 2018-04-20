b903c6581991655b33b9cbf609858db0845885d9
ESibrian <ESibrian@172.16.12.103>
2018-04-20 08:27:54 -0600

    Eliminacion de variables y descomentado de for

--- src/viernesabril20/Ejemplos.java
+++ src/viernesabril20/Ejemplos.java
@@ -5,17 +5,16 @@ import javax.swing.JOptionPane;
 
 public class Ejemplos {
     public static void main(String[] args) {
-        int[] edad;
-        edad = new int [10];
+        
         double [] precio = new double [3];
         double total = 0;
         
-        /*for (int i = 0; i < precio.length; i++) {
+        for (int i = 0; i < precio.length; i++) {
             precio[i]= Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese el precio: "));
             total += precio[i];
         }
         
-        JOptionPane.showMessageDialog(null,"El total es de: " + total);*/
+        JOptionPane.showMessageDialog(null,"El total es de: " + total);
         
         int seguir;
         String nombre;
